dd63fe39add664dd10ffada367e1d9f828216fae
randallust1024 <randallust1024@gmail.com>
2015-02-01 17:28:18 +0800

    Update to Boss

--- src/main/Boss.java
+++ src/main/Boss.java
@@ -1,12 +1,14 @@
 package main;
 
+import javafx.geometry.Point2D;
+
 public class Boss extends Target{
 	
 	public Boss(){
 	}
 	
-	public void summonZombie(Target[] zombie) {
-		for (int i = 0; i < zombie.length; i++) zombie[i].setVisible(true);
+	public void summonZombie(Target[] zombie, Point2D playerposition) {
+		for (int i = 0; i < zombie.length; i++) zombie[i].setVisible(playerposition);
 	}
 	
 }
