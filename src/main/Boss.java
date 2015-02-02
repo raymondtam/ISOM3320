936390e9c0dc096cacf51fc33e97f4e59e20acc0
rickyboy1113 <rickyboy1113@gmail.com>
2015-02-02 21:29:27 +0800

    boss constructor

--- src/main/Boss.java
+++ src/main/Boss.java
@@ -5,6 +5,13 @@ public class Boss extends Target{
 	public Boss(){
 	}
 	
+	public Boss(int health, int movingSpeed, int radius) {
+		this.setHealth(health);
+		this.setMovingSpeed(movingSpeed);
+		this.setRadius(radius);
+		//maxHealth = health;
+	}
+	
 	public void summonZombie(Target[] zombie) {
 		for (int i = 0; i < zombie.length; i++) zombie[i].setVisible(true);
 	}
