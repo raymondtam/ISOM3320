21382645723df794010cf94a79d9bb6575e182cc
rickyboy1113 <rickyboy1113@gmail.com>
2015-02-03 18:49:18 +0800

    delete public void main

--- src/main/Bullet.java
+++ src/main/Bullet.java
@@ -79,7 +79,7 @@ public class Bullet extends GameObject {
 		}
 	
 	public static Bullet[] getBulletArray(int size, int damage, int magazineSize, double radius, int speed) {
-		
+	
 		Bullet[] bulletArray = new Bullet [size];
 		for (int i = 0; i < bulletArray.length; i++){
 			bulletArray[i] = new Bullet (damage, magazineSize, radius, speed);
@@ -91,24 +91,6 @@ public class Bullet extends GameObject {
 	public String toString(){
 		return super.toString() + "bullet damage: " + bulletDamage + 
 				"magazine size: " + magazineSize + "\n";
-	}
-
-	public static void main (String[] args){
-		
-//		Bullet b1 = new Bullet(10,20);
-//		System.out.println("Bullet Damage is " + b1.getBulletDamage() + 
-//                   " and magazine size is " + b1.getMagazineSize());
-//		Bullet b2 = new Bullet (15, 15);
-//		System.out.println("Bullet Damage is " + b2.getBulletDamage() + 
-//                   " and magazine size is " + b2.getMagazineSize());
-//		Bullet b3 = new Bullet (20, 10);
-//		System.out.println("Bullet Damage is " + b3.getBulletDamage() + 
-//                   " and magazine size is " + b3.getMagazineSize());
-		
-		//Bullet [][] bulletArray = new Bullet[3][];
-		//bulletArray[0].length = b1.getMagazineSize();
-		//bulletArray[1].length = b2.getMagazineSize();
-		//bulletArray[2].length = b3.getMagazineSize();
 	}
 }
 
